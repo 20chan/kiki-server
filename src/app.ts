f9f533cd9a277e58c4125559503f9734a8541b89
20chan <2@0chan.dev>
2020-06-08 18:14:34 +0900

    cors 지원, router로 subpath 지원

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,17 @@
 import * as express from "express";
+import * as cors from "cors";
 import * as dbController from "./dbController";
 
-
-
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.set("port", process.env.PORT || 4000);
 
+var router = express.Router();
+router.get("/posts", dbController.allPosts);
+router.get("/communities", dbController.allCommunities);
 
-app.get("/", (req, resp) => {
-    resp.send('Hello, world!');
-});
-
-app.get("/posts", dbController.allPosts);
-app.get("/communities", dbController.allCommunities);
+app.use("/api/kiki", router);
 
 const server = app.listen(app.get("port"), () => {
     console.log("running on http://localhost:%d", app.get("port"));
